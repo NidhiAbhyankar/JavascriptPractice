62d78430fd674372a98855a6397eae21ae136996
Nihibhyankar <ndhibhyankargmail.com>
2020-11-30 12:43:07 +0530

    Blackjack in process

---
+++ OtherChallenges/js/script.js
@@ -0,0 +1,15 @@
+//challenge 5 :Blackjack
+let blackjackGame={
+  'you':{'scoreSpan':#your-blackjack-result,'div':#your-box,'score':0},
+  'dealer':{'scoreSpan':#dealer-blackjack-result,'div':#dealer-box,'score':0},
+}
+
+const YOU=blackjackGame['you'];
+const DEALER=blackjackGame['dealer'];
+
+document.querySelector('#blackjack-hit-button').addEventListener('click',blackjackHit);
+function blackjackHit(){
+  let cardImage=document.createElement('img');
+  cardImage.src='images/Q.png';
+  document.querySelector(YOU['div']).appendChild(cardImage);
+}
