2696ca40c546d6a11dad09ad716d68652d504b4c
NidhiAbhyka <nidhiabhyankar@mail.com>
2020-12-20 09:42:27 +0530

    unique values

--- OtherMethods/home.js
+++ OtherMethods/home.js
@@ -1,5 +1,6 @@
 //map method
 
+/*
 const people=[
   {
   name:'nidhi',
@@ -30,3 +31,35 @@ console.log(ages);
 const names=people.map((person)=>`<h2>${person.name}</h2>`);
 const result=document.querySelector('#result');
 result.innerHTML=names.join('');
+*/
+
+//unique values
+const menu=[
+  {
+    name:'oats',
+    category:'breakfast'
+  },
+  {
+    name:'sabji',
+    category:'lunch'
+  },
+  {
+    name:'rice',
+    category:'dinner'
+  },
+  {
+    name:'eggs',
+    category:'breakfast'
+  },
+  {
+    name:'sandwhich',
+    category:'breakfast'
+  }
+];
+
+const categories=['all',...new Set(menu.map((item)=>item.category))];  //convert it into set to grab unique values
+console.log(categories);                                         //and then ransform into an array
+const result=document.querySelector('.result');
+result.innerHTML=categories.map((category)=>{
+  return `<button>${category}</button>`;
+}).join('');
