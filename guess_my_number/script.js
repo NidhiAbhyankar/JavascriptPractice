afafd4fde48d9a29f3b3b4be26d75af1aeb6e987
Nidhi Abhyankar <nidhiabhyankar@mail.com>
2022-04-24 18:54:10 +0530

    Guess my number logic implementation

---
+++ guess_my_number/script.js
@@ -0,0 +1,37 @@
+'use strict'
+
+console.log(document.querySelector('.number').textContent)
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1
+// document.querySelector('.number').textContent = secretNumber
+
+let score = 20
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value)
+  console.log(guess, typeof guess)
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '🚫No Number!'
+  } else if (guess == secretNumber) {
+    document.querySelector('.message').textContent = '🎉Correct Message!'
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈Too High!'
+      score -= 1
+      document.querySelector('.score').textContent = score
+    } else {
+      document.querySelector('.message').textContent = 'You have lost the game!'
+      document.querySelector('.score').textContent = 0
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉Too Low!'
+      score -= 1
+      document.querySelector('.score').textContent = score
+    } else {
+      document.querySelector('.message').textContent = 'You have lost the game!'
+      document.querySelector('.score').textContent = 0
+    }
+  }
+})
